36de1b79c95d8a567b5d9b0f0176372721354379
Lorenz Moesenlechner <moesenle@gmail.com>
2017-07-08 09:58:48 +0200

    Got rid of trayer.

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ -f ~/.xmonad/trayer.pid ]; then
-    TRAYER_PID=$(cat ~/.xmonad/trayer.pid)
-    echo "b $TRAYER_PID $(ps -p $TRAYER_PID -o comm=)"
-    if [ "$(ps -p $TRAYER_PID -o comm=)" == "trayer" ]; then
-        kill $TRAYER_PID
-    fi
-fi
-
-trayer --edge top --align right --SetDockType true --SetPartialStrut true \
-       --expand true --width 6 --transparent true --tint 0x191970 --height 14 &
-echo "$!" > ~/.xmonad/trayer.pid
+#if [ -f ~/.xmonad/trayer.pid ]; then
+#    TRAYER_PID=$(cat ~/.xmonad/trayer.pid)
+#    echo "b $TRAYER_PID $(ps -p $TRAYER_PID -o comm=)"
+#    if [ "$(ps -p $TRAYER_PID -o comm=)" == "trayer" ]; then
+#        kill $TRAYER_PID
+#    fi
+#fi
+#
+#trayer --edge top --align right --SetDockType true --SetPartialStrut true \
+#       --expand true --width 6 --transparent true --tint 0x191970 --height 14 &
+#echo "$!" > ~/.xmonad/trayer.pid
 
 feh --bg-scale Pictures/wallpapers/cradle2k31600.jpg &
