b03984105144648edbd5fac570b9d610528325ed
Lorenz Moesenlechner <moesenle@gmail.com>
2017-07-08 09:58:47 +0200

    Run a start script when xmonad loads.
    The script currently only kills an old trayer, runs a new one and sets
    the background image.

--- xmonad.hs
+++ xmonad.hs
@@ -1,61 +1,39 @@
---
--- xmonad config file.
---
- 
--- XMonad Core
+
 import XMonad
-import System.Exit
- 
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
- 
--- GHC hierarchical libraries
-import XMonad.Operations
-import XMonad.Config
-import XMonad.Config.Gnome
-import XMonad.Util.Run
-import System.IO
-import Data.Ratio ((%))
- 
---Contribs
 import XMonad.Actions.CycleWS
-import XMonad.Actions.NoBorders
- 
+import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
- 
+import XMonad.Layout.ComboP
+import XMonad.Layout.Grid
 import XMonad.Layout.Maximize
+import XMonad.Layout.Named
 import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.SimplestFloat
 import XMonad.Layout.Tabbed
-import XMonad.Layout.ComboP
-import XMonad.Layout.Column    
 import XMonad.Layout.TwoPane
-import XMonad.Layout.Grid
-import XMonad.Layout.Named
-    
-import XMonad.Util.Loggers
+import XMonad.Util.Run
 import XMonad.Util.Themes
- 
--- modMask lets you specify which modkey you want to use. The default
--- is mod1Mask ("left alt").  You may also consider using mod3Mask
--- ("right alt"), which does not conflict with emacs keybindings. The
--- "windows key" is usually mod4Mask.
---
-myModMask	= mod4Mask
-altMask 	= mod1Mask 
- 
--- The default number of workspaces (virtual screens) and their names.
---
+import qualified XMonad.StackSet as W
+
+import Control.Monad (when, void)
+import System.Exit
+import System.IO
+import System.Directory (doesFileExist, getHomeDirectory)
+import System.FilePath.Posix ((</>))
+import System.Posix.Process (executeFile)
+import qualified Data.Map as M
+
+myModMask = mod4Mask
 myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
- 
+
 -- Border colors for unfocused and focused windows, respectively.
 --
-myNormalBorderColor  = "#000088"
+myNormalBorderColor = "#000088"
 myFocusedBorderColor = "#00dddd"
 
 ------------------------------------------------------------------------
@@ -64,46 +42,27 @@ myFocusedBorderColor = "#00dddd"
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     [ ((modMask,                xK_Return), spawn $ XMonad.terminal conf)
     , ((modMask,		xK_F1	), spawn "gmrun")
-    , ((modMask,	        xK_F2	), spawn "firefox")
+    , ((modMask,	        xK_F2	), spawn "chromium-browser")
     , ((modMask,	        xK_F3	), spawn "~/local/bin/ec")
     , ((modMask,	        xK_F4	), spawn "eclipse")
-    -- lock screen
     , ((modMask,	        xK_F12	), spawn "gnome-screensaver-command --lock")
-    -- close focused window
     , ((modMask .|. shiftMask,  xK_c    ), kill)
-     -- Rotate through the available layout algorithms
     , ((modMask,                xK_space ), sendMessage NextLayout)
-    -- Resize viewed windows to the correct size
     , ((modMask,                xK_r     ), refresh)
-    -- Move focus to the next window
     , ((modMask,                xK_j     ), windows W.focusDown)
-    -- Move focus to the previous window
     , ((modMask,                xK_k     ), windows W.focusUp  )
-    -- Maximize focused window.
     , ((modMask,             xK_m     ), withFocused $ sendMessage . maximizeRestore )
-    -- Swap the focused window and the master window
     , ((modMask .|. shiftMask,  xK_Return), windows W.swapMaster)
-    -- Swap window
-    , ((modMask,                     xK_o), sendMessage $ SwapWindow)
-    -- Swap the focused window with the next window
+    , ((modMask,                     xK_o), sendMessage SwapWindow)
     , ((modMask .|. shiftMask,  xK_j     ), windows W.swapDown  )
-    -- Swap the focused window with the previous window
     , ((modMask .|. shiftMask,  xK_k     ), windows W.swapUp    )
-    -- Shrink the master area
     , ((modMask,                xK_h     ), sendMessage Shrink)
-    -- Expand the master area
     , ((modMask,                xK_l     ), sendMessage Expand)
-    -- Push window back into tiling
     , ((modMask,                xK_t     ), withFocused $ windows . W.sink)
-    -- Increment the number of windows in the master area
     , ((modMask,                xK_comma ), sendMessage (IncMasterN 1))
-    -- Deincrement the number of windows in the master area
     , ((modMask,                xK_period), sendMessage (IncMasterN (-1)))
-    -- toggle the status bar gap
     , ((modMask,                xK_b     ), sendMessage ToggleStruts)
-    -- Quit xmonad (Default)
-    , ((modMask .|. shiftMask,  xK_q     ), io (exitWith ExitSuccess))
-    -- Restart xmonad
+    , ((modMask .|. shiftMask,  xK_q     ), io exitSuccess)
     , ((modMask .|. shiftMask,  xK_r     ),
           broadcastMessage ReleaseResources >> restart "xmonad" True)
     , ((modMask .|. controlMask, xK_k), screenWorkspace 1 >>= flip whenJust (windows . W.view))
@@ -112,7 +71,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask .|. shiftMask, xK_s), swapNextScreen)
     ]
     ++
- 
+
     --
     -- mod-[1..6], Switch to workspace N
     -- mod-shift-[1..6], Move client to workspace N
@@ -121,7 +80,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
     ++
- 
+
     --
     -- mod-{n,p}, Switch to {prev,next} workspace
     -- mod-shift-{n,p}, Move client and shift to {prev,next} workspace
@@ -132,7 +91,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
       ,	((modMask .|. shiftMask	, xK_p	), shiftToPrev >> prevWS)
     ,	((modMask .|. shiftMask	, xK_n	), shiftToNext >> nextWS)
     ]
- 
+
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
@@ -145,15 +104,15 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
- 
+
 ------------------------------------------------------------------------
 -- Layouts:
--- 
- 
+--
+
 genericLayout =	nameTail $ maximize $ smartBorders $
                 named "T" tiled
-                ||| named "TH" tiled_horizontal
 	        ||| named "M" (tabbed shrinkText (theme smallClean))
+                ||| named "TH" tiled_horizontal
 	        ||| named "F" simplestFloat
   where
      -- default tiling algorithm partitions the screen into two panes
@@ -165,11 +124,11 @@ genericLayout =	nameTail $ maximize $ smartBorders $
      ratio   = 1/2
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
- 
+
 myLayout = onWorkspace "1" (named "IM" $ combineTwoP
                             (TwoPane 0.01 0.15) Grid Grid (Const False)) $
            genericLayout
-  
+
 myManageHook = composeAll
     [ className =? "gmrun" --> doFloat,
       appName =? "desktop_window" --> doIgnore,
@@ -179,46 +138,44 @@ myManageHook = composeAll
       className =? "Unity-2d-launcher" --> doIgnore,
       appName =? "xfce4-notifyd" --> doIgnore
     ]
- 
+
 myLogHook :: Handle -> X ()
 myLogHook h =
   dynamicLogWithPP $ xmobarPP
                        {
                          ppOutput = hPutStrLn h,
-                         ppTitle = xmobarColor "green" "" . shorten 50
+                         ppTitle = xmobarColor "green" "" . shorten 200
                        }
- 
+
 ------------------------------------------------------------------------
- 
+
 startup :: X ()
 startup = do
   spawn "unity-settings-daemon"
-  spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut true \
-         \--expand true --width 6 --transparent true --tint 0x191970 --height 14"
 
 main :: IO ()
 main = do
+  executeInHome ".xmonad/start.sh"
   workspaceBarPipe <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
-                           
-  --  conkyBarPipe <- spawnPipe myConkyBar
-  xmonad $ ewmh $ withUrgencyHook NoUrgencyHook gnomeConfig {
-    -- simple stuff
-    terminal = "urxvt",
-    focusFollowsMouse = False,
-    borderWidth = 2,
-    modMask = myModMask,
-    workspaces = myWorkspaces,
-    normalBorderColor = myNormalBorderColor,
-    focusedBorderColor = myFocusedBorderColor,
- 
-    -- key bindings
-    keys = myKeys,
-    mouseBindings = myMouseBindings,
- 
-    -- hooks, layouts
-    manageHook = manageHook gnomeConfig <+> myManageHook <+> manageDocks,
-    logHook = logHook gnomeConfig <+> myLogHook workspaceBarPipe,
-    handleEventHook = fullscreenEventHook <+> ewmhDesktopsEventHook <+> handleEventHook gnomeConfig,
-    layoutHook = avoidStruts $ myLayout,
-    startupHook = startup <+> ewmhDesktopsStartup
-    }
+  xmonad $ ewmh $ withUrgencyHook NoUrgencyHook (config workspaceBarPipe)
+ where
+   config wp = gnomeConfig { terminal = "urxvt-wrapper"
+                           , focusFollowsMouse = False
+                           , borderWidth = 2
+                           , modMask = myModMask
+                           , workspaces = myWorkspaces
+                           , normalBorderColor = myNormalBorderColor
+                           , focusedBorderColor = myFocusedBorderColor
+                           , keys = myKeys
+                           , mouseBindings = myMouseBindings
+                           , manageHook = manageHook gnomeConfig <+> myManageHook <+> manageDocks
+                           , logHook = logHook gnomeConfig <+> myLogHook wp
+                           , handleEventHook = fullscreenEventHook <+> ewmhDesktopsEventHook <+> handleEventHook gnomeConfig
+                           , layoutHook = avoidStruts $ myLayout
+                           , startupHook = startup <+> ewmhDesktopsStartup
+                           , clickJustFocuses = False
+                           }
+
+   executeInHome file = do
+     home <- getHomeDirectory
+     doesFileExist (home </> file) >>= \e -> when e . void $ spawn (home </> file)
